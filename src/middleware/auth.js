7da8b5634e77941bd53bac0bd37f46301cea74a5
Stas Zaichenko <szaichenko@remedypointsolutions.com>
2022-06-05 12:53:58 +0400

    Test

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,15 +5,16 @@ const { User, Report } = model;
 
 module.exports = async (req, res, next) => {
     try {
-        const token = req.headers.authorization?.split(' ')[1];
-        const { email } = jwt.verify(token, 'TOKEN_SECRET');
-        const user = await User.findOne({where: { email }});
-        if (!user) {
-            return res.status(401).send({message: 'Пользователь не найден'});
-        }
-        const reports = await Report.findAll({where: { userId: user.id }});
-        req.user = user.dataValues;
-        req.user['reports'] = reports;
+        console.log('middleware')
+        // const token = req.headers.authorization?.split(' ')[1];
+        // const { email } = jwt.verify(token, 'TOKEN_SECRET');
+        // const user = await User.findOne({where: { email }});
+        // if (!user) {
+        //     return res.status(401).send({message: 'Пользователь не найден'});
+        // }
+        // const reports = await Report.findAll({where: { userId: user.id }});
+        // req.user = user.dataValues;
+        // req.user['reports'] = reports;
         next();
     } catch (e) {
         console.log(e)
