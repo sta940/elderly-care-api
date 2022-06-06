6b413356720ce22cd6fc150c6e15ec3516bf89c7
Stas Zaichenko <szaichenko@remedypointsolutions.com>
2022-06-06 12:14:27 +0400

    Revert middlewRE

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,16 +5,15 @@ const { User, Report } = model;
 
 module.exports = async (req, res, next) => {
     try {
-        console.log('middleware')
-        // const token = req.headers.authorization?.split(' ')[1];
-        // const { email } = jwt.verify(token, 'TOKEN_SECRET');
-        // const user = await User.findOne({where: { email }});
-        // if (!user) {
-        //     return res.status(401).send({message: 'Пользователь не найден'});
-        // }
-        // const reports = await Report.findAll({where: { userId: user.id }});
-        // req.user = user.dataValues;
-        // req.user['reports'] = reports;
+        const token = req.headers.authorization?.split(' ')[1];
+        const { email } = jwt.verify(token, 'TOKEN_SECRET');
+        const user = await User.findOne({where: { email }});
+        if (!user) {
+            return res.status(401).send({message: 'Пользователь не найден'});
+        }
+        const reports = await Report.findAll({where: { userId: user.id }});
+        req.user = user.dataValues;
+        req.user['reports'] = reports;
         next();
     } catch (e) {
         console.log(e)
